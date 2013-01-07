11d888d57dedfd1f3765abab240cabede0a61907
cainmd <cainmd@uab.edu>
2013-01-07 12:57:02 -0600

    update java

--- javaCode.js
+++ javaCode.js
@@ -1,15 +1,15 @@
 var alertme = function () {
-    alert(parseInt(FL.value) + parseInt(HL.value) );
+    alert(parseInt(FL.value) + CR );
     //alert("help");
 }
 
 var form1 = document.getElementById("form1");
 
-var confirm = document.getElementById("confirm");
+var submit = document.getElementById("submit");
 //var flinput = document.getElementById("flInput");
 //var crinput = document.getElementById("crInput");
 var FL = document.getElementById("FL");
-var HL = document.getElementById("HL");
+var CR = parseInt(document.getElementById("CR").value);
 
 submit.onclick = alertme;
 
