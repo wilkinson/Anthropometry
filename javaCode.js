4a411172a2901b5d07c328c4bbe7f1e6e9d9faec
cainmd <cainmd@uab.edu>
2013-01-07 13:01:10 -0600

    Update index 2

--- javaCode.js
+++ javaCode.js
@@ -1,5 +1,5 @@
 var alertme = function () {
-    alert(parseInt(FL.value) + CR );
+    alert(parseInt(FL.value) + parseInt(CR.value) );
     //alert("help");
 }
 
@@ -9,7 +9,20 @@ var submit = document.getElementById("submit");
 //var flinput = document.getElementById("flInput");
 //var crinput = document.getElementById("crInput");
 var FL = document.getElementById("FL");
-var CR = parseInt(document.getElementById("CR").value);
+var CR = document.getElementById("CR");
+var CH = document.getElementById("CH");
+var HC = document.getElementById("HC");
+var CC = document.getElementById("CC");
+var AC = document.getElementById("AC");
+var bodyWeight = document.getElementById("BodyWeight");
+var BW = document.getElementById("BW");
+var LVW = document.getElementById("LVW");
+var LUW = document.getElementById("LUW");
+var HW = document.getElementById("HW");
+var TW = document.getElementById("TW");
+var SW = document.getElementById("SW");
+var KW = document.getElementById("KE");
+var AW = document.getElementById("AW");
 
 submit.onclick = alertme;
 
